14da38965c627fe3173dc0d3a90af39c1eb119d1
Simon Hoefling <simon@hoeflia.eu>
2023-04-15 13:15:50 +0800

    Added pulsing circles for hero section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen">
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Head>
         <title>Simon's Portfolio</title>
       </Head>
@@ -15,8 +15,8 @@ export default function Home() {
       <Header />
 
       {/* Hero */}
+      <section id="hero" className="snap-center">
         <Hero />
-      <section id="hero">
       </section>
       {/* About */}
 
