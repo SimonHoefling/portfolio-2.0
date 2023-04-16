e7ea2854ab069ade421eb10baed7ac005a03402f
Simon Hoefling <simon@hoeflia.eu>
2023-04-16 21:35:33 +0800

    Fixed skills hover in on phone screen

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -11,13 +11,13 @@ export default function Skill({ directionLeft, imagePath }: Props) {
     <div className='group relative flex'>
       <motion.img
         initial={{
-          x: directionLeft ? -200 : 200,
+          x: directionLeft ? -100 : 100,
           opacity: 0,
         }}
         transition={{ duration: 1 }}
         whileInView={{ opacity: 1, x: 0}}
         src={imagePath}
-        className='rounded-full border border-gray-500 object-cover w-24 h-24'
+        className='rounded-full border border-gray-500 object-cover w-16 h-16 md:w-24 md:h-24'
       />
     </div>
   );
