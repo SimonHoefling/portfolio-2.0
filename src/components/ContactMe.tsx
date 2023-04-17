afcb59316a11ecd5fe49d7859568e1726c419dc4
Simon Hoefling <simon@hoeflia.eu>
2023-04-17 16:45:11 +0800

    Made Contact section responsive

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -25,39 +25,40 @@ export default function ContactMe({}: Props) {
         Contact
       </h3>
 
-      <div className='flex flex-col space-y-10'>
-        <h4 className='text-4xl font-semibold text-center'>
+      <div className='flex flex-col mt-32 space-y-10'>
+        <h4 className='text-2xl font-semibold text-center lg:text-4xl'>
           I have got just what you need.{" "}
           <span className='decoration-[#F7AB0A]/50 underline'>Let's talk.</span>
         </h4>
       </div>
 
       <div className='space-y-10'>
-        <div className='flex items-center space-x-5 justify-center'>
-          <PhoneIcon className="h-7 w-7 text-[#F7AB0A] animate-pulse" />
-          <p className='text-2xl'>+62 819 354 504 23</p>
-        </div>
-        <div className='flex items-center space-x-5 justify-center'>
+        <div className='flex items-center space-x-5'>
           <EnvelopeIcon className="h-7 w-7 text-[#F7AB0A] animate-pulse" />
-          <p className='text-2xl'>simon@hoeflia.eu</p>
+          <p className='text-1xl'>simon@hoeflia.eu</p>
         </div>
-        <div className='flex items-center space-x-5 justify-center'>
+        <div className='flex items-center space-x-5'>
+          <PhoneIcon className="h-7 w-7 text-[#F7AB0A] animate-pulse" />
+          <p className='text-1xl'>+62 819 354 504 23</p>
+        </div>
+        <div className='flex items-center space-x-5'>
           <MapPinIcon className="h-7 w-7 text-[#F7AB0A] animate-pulse" />
-          <p className='text-2xl'>97737 Gemuenden / Germany</p>
+          <p className='text-1xl'>97737 Gemuenden / Germany</p>
         </div>
 
         <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col space-y-2 w-fit mx-auto'>
-          <div className='space-x-2'>
+          <div className='space-y-2'>
             <input
               {...register('name')}
               placeholder='Name'
-              className='contactInput'
+              className='contactInput w-full px-4 py-2'
               type="text"
+              // px-6 py-4
             />
             <input
               {...register('email')}
               placeholder='Email'
-              className='contactInput'
+              className='contactInput w-full px-4 py-2'
               type="email"
             />
           </div>
@@ -65,17 +66,18 @@ export default function ContactMe({}: Props) {
           <input
             {...register('subject')}
             placeholder='Subject'
-            className='contactInput'
+            className='contactInput px-4 py-2'
             type="text"
           />
           <textarea
             {...register('message')}
             placeholder='Message'
-            className='contactInput'
+            className='contactInput px-4 py-2'
           />
           <button
             type='submit'
-            className='bg-[#F7AB0A] py-5 px-10 rounded-md text-black font-bold text-lg'>
+            className='bg-[#F7AB0A] py-3 px-6 rounded-md text-black font-bold text-lg'>
+              {/* py-5 px-10 */}
             Submit
           </button>
         </form>
